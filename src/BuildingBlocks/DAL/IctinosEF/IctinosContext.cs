fa3d18079d8eb3d52fca95f14951f71e263b9d16
Dashell_J <alain.galas@ineat-conseil.fr>
2019-12-10 00:11:59 +0100

    init(): setup projetcs arbo

---
+++ src/BuildingBlocks/DAL/IctinosEF/IctinosContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IctinosEF
+{
+    public class IctinosContext : DbContext
+    {
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+        public IctinosContext(DbContextOptions<IctinosContext> options)
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+              : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}

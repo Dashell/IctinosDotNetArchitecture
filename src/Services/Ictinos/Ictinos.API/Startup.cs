6f0ff1c92eb0b094f8d6f39a4e188802d8532c83
Dashell_J <alain.galas@ineat-conseil.fr>
2020-01-13 22:40:28 +0100

    feat(): poc nuget package local

--- src/Services/Ictinos/Ictinos.API/Startup.cs
+++ src/Services/Ictinos/Ictinos.API/Startup.cs
@@ -1,3 +1,5 @@
+using CoinPaprika.Consumer;
+using CoinPaprika.Consumer.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,18 +14,18 @@ namespace Ictinos.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCoinPaprikaClient(null);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ICoinPaprikaClient iCoinPaprikaClient) 
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
+            var test = iCoinPaprikaClient.GetCoinsAsync().Result;
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
